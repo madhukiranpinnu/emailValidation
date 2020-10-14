4f3f2ce0153c8a62084da0d4b2a538e6f323daef
madhukiranpinnu <madhukiran918@gmail.com>
2020-10-14 10:07:19 +0530

    emiail validation for 2 mandatory cases

--- emailValidation.sh
+++ emailValidation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "welcome to email validtion program"
+
+read -p "enter email please: " email 
+pattern="^[A-Za-z0-9]+.[a-zA-Z]{2,4}"
+
+if [[ $email =~ $pattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
